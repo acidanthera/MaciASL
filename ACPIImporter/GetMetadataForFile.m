9af839dc9518874cf8130b6230ba17966e57f182
phpdev32 <firewater2311@yahoo.com>
2013-09-22 16:29:43 -0400

    Apply metadata and clean up main method

--- ACPIImporter/GetMetadataForFile.m
+++ ACPIImporter/GetMetadataForFile.m
@@ -89,38 +89,28 @@ Boolean GetMetadataForFile(void *thisInterface, CFMutableDictionaryRef attribute
 
     Boolean ok = FALSE;
     @autoreleasepool {
-        NSError *error = nil;
         
         if ([(__bridge NSString *)contentTypeUTI isEqualToString:@"org.acpica.aml"]) {
             // import from store file metadata
             
-            // Create the URL, then attempt to get the meta-data from the store
-            NSURL *url = [NSURL fileURLWithPath:(__bridge NSString *)pathToFile];
-            
-            // If there is no error, add the info
-            if (error == NULL) {
                 // Get the information you are interested in from the dictionary
                 // "YOUR_INFO" should be replaced by key(s) you are interested in
                 
-                NSArray *contentToIndex = StringsForAML([NSData dataWithContentsOfURL:url]);
-                if (contentToIndex != nil) {
-                    // Add the metadata to the text content for indexing
-                    ((__bridge NSMutableDictionary *)attributes)[(NSString *)kMDItemTextContent] = [contentToIndex componentsJoinedByString:@" "];
-                    ok = TRUE;
-                }
-            }
+                ok = MetadataForAML([NSData dataWithContentsOfFile:(__bridge NSString *)pathToFile], attributes);
             
         } else if ([(__bridge NSString *)contentTypeUTI isEqualToString:@"net.sourceforge.maciasl.tableset"]) {
             // import from an external record file
             
             NSMutableArray *strings = [NSMutableArray array];
-            NSDictionary *tables = [[NSDictionary dictionaryWithContentsOfFile:(__bridge NSString *)pathToFile] objectForKey:@"Tables"];
+            NSDictionary *tables = [NSDictionary dictionaryWithContentsOfFile:(__bridge NSString *)pathToFile];
+            CFDictionaryAddValue(attributes, kMDItemSubject, (__bridge CFStringRef)[tables objectForKey:@"Hostname"]);
+            tables = [tables objectForKey:@"Tables"];
             for (NSString *table in tables) {
                 [strings addObject:table];
                 [strings addObjectsFromArray:StringsForAML([tables objectForKey:table])];
             }
             
-            ((__bridge NSMutableDictionary *)attributes)[(NSString *)kMDItemTextContent] = [strings componentsJoinedByString:@" "];
+            CFDictionaryAddValue(attributes, kMDItemTextContent, (__bridge CFStringRef)[strings componentsJoinedByString:@" "]);
             ok = TRUE;
         }
     }
