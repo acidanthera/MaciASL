6356efa2ecc7d40c890aef1bd24e5fdb105a7dbf
phpdev32 <firewater2311@yahoo.com>
2014-01-08 22:52:26 -0500

    Add tentative top-level exception handler

--- MaciASL/main.m
+++ MaciASL/main.m
@@ -8,8 +8,24 @@
 
 #import <Cocoa/Cocoa.h>
 
+void handle_exception(NSException *exception) {
+    @try {
+        NSString *file = [NSString stringWithFormat:@"/tmp/%ld.plist", roundtol([[NSDate date] timeIntervalSince1970])];
+        NSMutableDictionary *d = [NSMutableDictionary dictionary];
+        if (exception.name) [d setObject:exception.name forKey:@"name"];
+        if (exception.reason) [d setObject:exception.reason forKey:@"reason"];
+        if (exception.userInfo) [d setObject:exception.userInfo forKey:@"userInfo"];
+        if (exception.callStackReturnAddresses) [d setObject:exception.callStackReturnAddresses forKey:@"callStackReturnAddresses"];
+        if (exception.callStackSymbols) [d setObject:exception.callStackSymbols forKey:@"callStackSymbols"];
+        [d writeToFile:file atomically:true];
+        NSRunAlertPanel(@"Uncaught Exception", @"An uncaught exception has occurred, and the program will now terminate. Please post the revealed file (may contain personal information), to %@", nil, nil, nil, @"http://sourceforge.net/projects/maciasl/support");
+        [NSWorkspace.sharedWorkspace selectFile:file inFileViewerRootedAtPath:file];
+    } @catch (NSException *e) {}
+}
+
 int main(int argc, char *argv[])
 {
+    NSSetUncaughtExceptionHandler(&handle_exception);
     NSImage *temp = [[NSImage alloc] initByReferencingFile:@"/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/ToolbarUtilitiesFolderIcon.icns"];
     temp.name = @"ToolbarUtilitiesFolderIcon";
     return NSApplicationMain(argc, (const char **)argv);
